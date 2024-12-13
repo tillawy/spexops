b52beca8e63629d81ab488d651a2fedebf14965a
Mohammed O. Tillawy <tillawy@gmail.com>
2024-12-13 11:43:50 +0300

    fix info_controller.rb

--- app/controllers/api/app/info_controller.rb
+++ app/controllers/api/app/info_controller.rb
@@ -1,7 +1,7 @@
 class Api::App::InfoController < ApplicationController
   def index
-    package_json_file = Rails.root.join("package.json").read
-    json = JSON.parse(package_json_file)
+    info_json_file = Rails.root.join("info.json").read
+    json = JSON.parse(info_json_file)
     render json: { version: json["version"], commit: json["sha"] || "n/a" }
   end
 end
