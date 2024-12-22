6cbc1757116f91ab0c72dd254f40003f6f2d67ef
Mohammed O. Tillawy <tillawy@gmail.com>
2024-12-22 20:24:51 +0300

    appliction_controller, skip csrf verification for JSON requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   include Errors::ErrorHandler
+
+  protect_from_forgery with: :exception
+  skip_before_action :verify_authenticity_token, if: :json_request?
+
+  private
+
+  def json_request?
+    request.format.json?
+  end
 end
